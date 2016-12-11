6da5cb96d150c291d9c162e9165e65fe2604dc49
danlurie <danjlurie@gmail.com>
2016-12-10 19:56:59 -0800

    Half way through the FAB Views tutorial. Next up, FormViews

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Brain Damage Patient Database (BDPDB)
-BDPDB is a browser-based application for managing, browsing, and searching demographic information and neuroimaging data from patients with brain injury.
-
-This application is intended for use by researchers working with fully anonymized data, and **is not designed for use in clinical settings**. 
+BDPDB is a browser-based application for managing, browsing, and searching demographic information and neuroimaging data from patients with brain injury. This application is intended for use by researchers working with fully anonymized data, and **is not designed for use in clinical settings**. Database access can be limited to authorized users.
--- bdpdb/app/views.py
+++ bdpdb/app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
-from flask.ext.appbuilder import ModelView
+from flask.ext.appbuilder import ModelView, AppBuilder, expose, BaseView, has_access
 from app import appbuilder, db
 
 """
@@ -16,6 +16,40 @@ from app import appbuilder, db
 
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
+
+"""
+Example views from http://flask-appbuilder.readthedocs.io/en/latest/views.html
+"""
+class MyView(BaseView):
+    
+    default_view = 'method1'
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # return param1
+        return "Hello, authorized user."
+    
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # render param1
+        param1 = "Goodbye, {}.".format(param1)
+        return param1
+
+    #from flask import render_template
+
+    @expose('/method3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # render param1 in the template
+        param1 = "Then {} said: 'Hey there, good looking.'".format(param1)
+        self.update_redirect()
+        return self.render_template('method3.html', param1=param1)
+
+appbuilder.add_view(MyView, 'Method1', category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/Dan', category='My View')
+appbuilder.add_link("Method3", href='/myview/method3/Dan', category='My View')
 
 """
     Application wide 404 error handler
